b92946f2ccf9aec55b0f0dc467f30f4eb2ec5a48
Manopalaniraja21 <127928332+Manopalaniraja21@users.noreply.github.com>
2023-11-23 12:25:38 +0530

    updated App.js

--- App.jsx
+++ App.jsx
@@ -1,23 +1,27 @@
-import './App.css'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Login from './components/Login/Login'
-import Registration from './components/Registration/Registration'
-import Dashboard from './DashBoard/DashBoard'
+// App.jsx
+import React from 'react';
+import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Login from './components/Login/Login';
+import Registration from './components/Registration/Registration';
+import Dashboard from './DashBoard/DashBoard';
+import PrivateRouter from './components/PrivateRouter/PrivateRouter';
+import ParentComponent from './components/Memo/ReactMemoHook';
+import CounterComponent from './components/Memo/ReactMemoHook';
 
 function App() {
-
-
   return (
     <>
       <BrowserRouter>
         <Routes>
           <Route path='/login' element={<Login />} />
           <Route path='/register' element={<Registration />} />
-          <Route path='/dashboard' element={<Dashboard />} />
+          <Route path='/dashboard' element={<PrivateRouter element={<Dashboard />} />} />
+          <Route path='/memo' element={<CounterComponent />} />
         </Routes>
       </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
