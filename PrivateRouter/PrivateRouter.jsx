95704d8bef1d45984c5ee0e66cdc6259d6fdb66b
Manopalaniraja21 <127928332+Manopalaniraja21@users.noreply.github.com>
2023-11-23 12:24:07 +0530

    Private Routing -Private Router
    Use of private router component is given here

---
+++ PrivateRouter/PrivateRouter.jsx
@@ -0,0 +1,14 @@
+// PrivateRouter.jsx
+import React, { useContext } from "react";
+import { Navigate, Route } from "react-router-dom";
+import UserContext from "../../Context/UserContext";
+
+const PrivateRouter = ({ element, ...props }) => {
+    const { user } = useContext(UserContext);
+
+    return user ?
+        <Route {...props} element={element} />
+        : <Navigate to="/login" />;
+};
+
+export default PrivateRouter;
